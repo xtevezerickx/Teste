0d7ed3da3bc9e615346cf71738ea1c4061219512
erick.silva <erick.silva@CONTW-DES18.CONTMATIC.AD>
2017-04-24 13:43:30 -0300

    commit 2

--- ProjetoTeste/src/br/com/contmatic/teste/Pessoa.java
+++ ProjetoTeste/src/br/com/contmatic/teste/Pessoa.java
@@ -3,6 +3,8 @@ package br.com.contmatic.teste;
 public class Pessoa {
 
     private Integer idade;
+    
+    private String nome;
 
     public Integer getIdade() {
         return idade;
@@ -10,6 +12,14 @@ public class Pessoa {
 
     public void setIdade(Integer idade) {
         this.idade = idade;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     @Override
